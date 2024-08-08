46713990f0cf462261c8710c1fc85da4b3e1f35c
Benil Jose Kaiyanickal <benil@keyvalue.systems>
2024-08-08 17:50:28 +0530

    implement first test

---
+++ src/js/__tests__/formInput.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import FormInput from '../components/form-input/formInput';
+
+describe("FormInput component", () => {
+    test("Test run", () => {
+        expect(0).toEqual(0);
+    });
+});
